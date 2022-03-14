46938ac5f266302a54249df83187bd77ee7b08a8
ammar <88188033+ammar-abyte@users.noreply.github.com>
2022-03-14 12:48:04 +0500

    Update Splash.kt

--- app/src/main/java/com/muhammad_ammar/tentwenty/view/activities/Splash.kt
+++ app/src/main/java/com/muhammad_ammar/tentwenty/view/activities/Splash.kt
@@ -16,7 +16,7 @@ class Splash : BaseActivity() {
             val mIntent = Intent(this@Splash, MainActivity::class.java)
             startActivity(mIntent)
             finish()
-        }, 100)
+        }, 2000)
 
     }
 
